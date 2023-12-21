919e348839999b9ab64d0f6378e65fa4925ce673
Joy Dutta <dutta_joy@hotmail.com>
2023-12-21 16:13:57 +0000

    3/02. Middleware Chain

--- MiddlewareExample/Program.cs
+++ MiddlewareExample/Program.cs
@@ -1,11 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.Run( async (HttpContext context) => {
+//middlware 1
+app.Use(async (context, next) => {
     await context.Response.WriteAsync("Hello");
+    await next(context);
 });
 
-app.Run( async(HttpContext context) => {
-    await context.Response.WriteAsync("Hello Again");
+//middleware 2
+app.Use(async (context, next) => {
+    await context.Response.WriteAsync("Hello again");
+    await next(context);
 });
+
+//middleware 3
+app.Run(async (HttpContext context) => {
+    await context.Response.WriteAsync("Hello again");
+});
+
+
 app.Run();
